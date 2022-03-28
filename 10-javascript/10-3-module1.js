d9a0873a8c796ef60fde94189d7a6ada2f813b82
TedJunny <hayejun1013@naver.com>
2022-03-28 11:37:00 +0900

    chore: add typescript study note #10

---
+++ 10-javascript/10-3-module1.js
@@ -0,0 +1,8 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export let number = 10;
+export function print() {
+  console.log("print");
+}
---
+++ 10-javascript/10-3-module2.js
@@ -0,0 +1,5 @@
+import * as calculator from "./10-3-module1.js";
+
+console.log(calculator.add(2, 5));
+calculator.print();
+calculator.number;
